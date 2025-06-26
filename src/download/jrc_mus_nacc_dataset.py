cfca9a3164a4a8a362dd1d336cc82a5856f9b1e2
Helen Cristina <helensilva14@gmail.com>
2025-06-26 12:05:27 -0400

    Implement JRC MUS dataset download

--- src/download/jrc_mus_nacc_dataset.py
+++ src/download/jrc_mus_nacc_dataset.py
@@ -1,11 +1,16 @@
-DATASET_URL = "s3://janelia-cosem-datasets/jrc_mus-nacc-2/jrc_mus-nacc-2.zarr"
-SAVE_PATH = "data/raw/jrc_mus_nacc_2.zarr"
+import quilt3 as q3
+
+BUCKET_ROOT = "s3://janelia-cosem-datasets"
+INTERNAL_PATH = "jrc_mus-nacc-2/jrc_mus-nacc-2.zarr/recon-2/em/fibsem-int16/"
+SAVE_PATH = "data/raw/jrc_mus_nacc_2.zarr/"
+
 METADATA_FILE = "outputs/jrc_mus_nacc_2_metadata.json"
 ALL_METADATA_FILE = "outputs/jrc_mus_nacc_2_all_metadata.json"
 
 def download_dataset():
     """Downloads the Janelia Mouse nucleus accumbens dataset."""
-    pass
+    bucket = q3.Bucket(BUCKET_ROOT)
+    bucket.fetch(INTERNAL_PATH, SAVE_PATH)
 
 def extract_metadata():
     """Extracts metadata from the downloaded Zarr file and saves it to a JSON file."""
